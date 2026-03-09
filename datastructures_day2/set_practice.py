4e78f3dd0ed54bb534cbff1090f71d9ba2f069f4
Ines Heiland <ines.heiland@uit.no>
2026-03-09 20:37:22 +0100

    added exercises for day 2

---
+++ datastructures_day2/set_practice.py
@@ -0,0 +1,26 @@
+"""
+set_practice.py
+
+In this exercise you will practice with set methods
+
+Follow the instructions below
+"""
+
+# create a set of colors: red, green, blue
+
+
+# add the color yellow to this set using the add method
+
+
+
+# update the set with the colors purple and pink with the update method
+
+
+
+# make a list out of this set
+
+
+# remove the 6 from the set using the discard method
+
+
+# Make two sets, find their intersection, union, and difference
